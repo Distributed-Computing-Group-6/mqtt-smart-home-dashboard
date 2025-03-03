76d7686332f468a909079a04b9d0a1cefb933dac
Ambrgna <mittens.pritchett@gmail.com>
2025-03-03 16:30:08 -0600

    Update dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -16,12 +16,13 @@ export class DashboardComponent implements OnInit {
     this.mqttService.getDevices().subscribe(devices => {
       this.devices = devices.map((device: { friendly_name: string, definition?: { exposes: any[]} }) => {
         const exposes = device.definition?.exposes || [];
-        const features = exposes.flatMap(expose => expose.features || []);
+        const features = exposes?.flatMap(expose => expose.features || []);
+        const controls = features.length > 0 ? features : exposes.slice(0, 1) || [];        
         const type = exposes.length > 0 ? exposes[0].type : null; 
 
         return {
           friendly_name: device.friendly_name,
-          features: features,
+          features: controls,
           type: type,
           exposes: exposes.slice(1)
         };
