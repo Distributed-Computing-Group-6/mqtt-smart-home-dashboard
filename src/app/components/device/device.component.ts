c7fdb1e300ce628336f67bd27a5ca49f2b15b884
Joshua Pritchett <mittens.pritchett@gmail.com>
2025-03-25 00:16:50 -0500

    adding device edit and delete

--- src/app/components/device/device.component.ts
+++ src/app/components/device/device.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, TemplateRef } from '@angular/core';
+import { Component, Inject, Input, TemplateRef, ViewChild } from '@angular/core';
 import { MqttService } from '../../services/mqtt.service';
 import { NgIfContext } from '@angular/common';
 
@@ -7,17 +7,50 @@ import { NgIfContext } from '@angular/common';
   templateUrl: './device.component.html',
   styleUrl: './device.component.css'
 })
-export class DeviceComponent {
+export class DeviceComponent { 
+  @ViewChild('editModalClose') editClose: any;
   @Input() device!: any;
-  topic!: string;
-  elseTemplate!: TemplateRef<NgIfContext<boolean>> | null ;
   @Input() isCard: boolean = true;
+  topic!: string;
+  baseTopic!: string;
+  elseTemplate!: TemplateRef<NgIfContext<boolean>> | null ;
+  isInvalid: boolean = false;
+  invalidMessage!: string;
 
   constructor(public mqttService: MqttService) {}
 
   ngOnInit() {
-    console.log(this.isCard);
-    this.topic = `${this.mqttService.getBaseTopic()}/${this.device.friendly_name}`;
+    this.baseTopic =`${this.mqttService.getBaseTopic()}`;
+    this.topic = `${this.baseTopic}/${this.device.friendly_name}`;
   }
-  
+
+  editName(event: Event,newName:string){  
+    event.preventDefault();
+
+    const stateTopic:string = `${this.baseTopic}/bridge/request/device/rename`;
+    let message = {
+      from: this.device.friendly_name,
+      to: newName.trim()
+    };
+    console.log(message);
+    this.mqttService.publish(stateTopic,JSON.stringify(message));
+    
+    this.mqttService.getUpdate(`${this.baseTopic}/bridge/response/device/rename`, "", (value) => {
+      if(value.error){
+        console.log(value.error);
+        this.isInvalid = true;
+        this.invalidMessage = value.error;
+      } else {
+        this.editClose.nativeElement.click();
+        this.isInvalid = false;
+      }
+    });
+  }
+
+  deleteDevice(){
+    const stateTopic:string = `${this.baseTopic}/bridge/request/device/remove`;
+    let message = this.device.friendly_name;
+    console.log(message);
+    this.mqttService.publish(stateTopic,message);
+  }
 }
--- src/app/components/group/group-card/group-card.component.ts
+++ src/app/components/group/group-card/group-card.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MqttService } from '../../../services/mqtt.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+declare var $: any;
 
 @Component({
   selector: 'app-group-card',
@@ -7,13 +10,54 @@ import { MqttService } from '../../../services/mqtt.service';
   styleUrl: './group-card.component.css'
 })
 export class GroupCardComponent {
+  @ViewChild('editModalClose') editClose: any;
   @Input() group!: any;
   topic!: string;
+  isEdit: boolean = true;
+  baseTopic!: string;
+  isInvalid: boolean = false;
+  invalidMessage!: string;
   
-  constructor(public mqttService: MqttService) {}
+  constructor(public mqttService: MqttService, private modalService: NgbModal) {}
 
   ngOnInit() {
-    this.topic = `${this.mqttService.getBaseTopic()}/${this.group.friendly_name}`;
+    this.baseTopic =`${this.mqttService.getBaseTopic()}`;
+    this.topic = `${this.baseTopic}/${this.group.friendly_name}`;
+  }
+  
+  editName(event: Event,newName:string){  
+    event.preventDefault();
+
+    const stateTopic:string = `${this.baseTopic}/bridge/request/group/rename`;
+    let message = {
+      from: this.group.friendly_name,
+      to: newName.trim()
+    };
+    console.log(message);
+    this.mqttService.publish(stateTopic,JSON.stringify(message));
+    
+    this.mqttService.getUpdate(`${this.baseTopic}/bridge/response/group/rename`, "", (value) => {
+      if(value.error){
+        console.log(value.error);
+        this.isInvalid = true;
+        this.invalidMessage = value.error;
+      } else {
+        this.editClose.nativeElement.click();
+        this.isInvalid = false;
+      }
+    });
+  }
+
+  deleteGroup(){
+    const stateTopic:string = `${this.baseTopic}/bridge/request/group/remove`;
+    let message = this.group.friendly_name;
+    console.log(message);
+    this.mqttService.publish(stateTopic,message);
+  }
+
+  closeModal() {
+    // You need to get the modal reference and close it
+    this.modalService.dismissAll();  // This closes all open modals
   }
   
 }
